cc64ce836f0710641dbb332119184b116949c8c3
lukitassukitas <raphaelcamara29@gmail.com>
2023-02-13 22:25:08 -0300

    JS

---
+++ react-aula01/string.js
@@ -0,0 +1,17 @@
+const escola = "cod3r"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+
+console.log(escola.charAt(3))
+console.log(escola.indexOf('3'))
+
+console.log(escolaa.substring(1))
+console.log(escolaa.substring(0,3))
+console,log('Escola' .concat(escola).concat('!'))
+
+console.log(escola.replace(3,'e'))
+console.log(escola.replace(/\d/, 'e'))
+
+console.log('Ana, Lucas, Pedro'.split(', '))
+
---
+++ react-aula01/tipagemfraca.js
@@ -0,0 +1,13 @@
+let qualquer = ["Raphael","santos"] 
+let qualquer2 = { 
+    nome: "Raphael",
+    idade: 17
+}
+console.log(qualquer)
+console.log(Array.IsArray(qualquer))
+console.log(qualquer2)
+console.log(typeof qualquer2)
+
+// qualquer = 3.14
+// console.log (qualquer)
+// console.log (typeof qualquer)
